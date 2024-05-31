dc2b60d5c9e2b6456d68535a9e2030748a7bb7d7
christopherjimenez <jimenezchristopher5@gmail.com>
2024-05-30 20:23:25 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  return (
+    <>
+      <NavBar user={user} />
+      <h1>Hello world!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+
+const NavBar = ({ user }) => {
+    return (
+      <>
+        <p>Hello Navbar</p>
+      </>
+    )
+  }
+  export default NavBar
+  
